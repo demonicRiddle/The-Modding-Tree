7a74fd6581238574c7f7f9c8edb550c5e2872b64
demonicRiddle <36573812+demonicRiddle@users.noreply.github.com>
2020-11-02 23:51:01 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -6,7 +6,7 @@ addLayer("w", {
             unlocked: true,
 			points: new Decimal(0),
         }},
-        color: "#2a72a7",
+        color: "#2a72a8",
         requires: new Decimal(10), // Can be a function that takes requirement increases into account
         resource: "wacky points", // Name of prestige currency
         baseResource: "points", // Name of resource prestige is based on
